7237b31a4d7ca51463f91b978413c59fb76efce4
hkyss <hkyss.services@protonmail.com>
2021-09-08 12:20:18 +0300

    Add plugin

---
+++ assets/plugins/EvoUserReplaceQuotes/plugin.EvoUserReplaceQuotes.php
@@ -0,0 +1,23 @@
+<?php
+include_once MODX_BASE_PATH.'assets/lib/MODxAPI/modUsers.php';
+$modUsers = new modUsers($modx);
+$e = &$modx->event;
+
+if ($e->name == 'OnWUsrFormSave') {
+  $modUsers->edit((int)$id);
+
+  $result = $modx->db->query('SELECT * FROM '.$modx->getFullTableName('web_user_attributes').' WHERE internalKey = '.(int)$id);
+  while($row = $modx->db->getRow($result)) {
+    if(!empty($row)) {
+      foreach($row as $item_key => $item) {
+        $item = str_replace('\'','"',$item);
+        $item = str_replace('"', '»', preg_replace('/((^|\s)"(\w))/um', '\2«\3', $item));
+        $modUsers->set($item_key,$item);
+      }
+    }
+  }
+  $modUsers->save(false,false);
+  $modUsers->close();
+}
+
+unset($modUsers);
